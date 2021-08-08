b7cd65392158303ab39e8e0e1b8aed33304617bb
kakkarmanan <mr.manankakkar@gmail.com>
2021-08-08 16:50:48 +0530

    Admin and Employee Profile Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HomePage from "./components/pages/HomePage";
 import LandingPage from "./components/pages/LandingPage";
 import LoginPage from "./components/pages/LoginPage";
 import RegistrationPage from "./components/pages/RegistrationPage";
+import EmployeeProfilePage from './components/employeeProfilePage';
+import AdminProfilePage from './components/adminPage'
 
 //This project is used in a hackathon
 
@@ -14,6 +16,8 @@ const App = () => (
   <Route path="/login" exact component = {LoginPage}/>
   <Route path="/registration" exact component = {RegistrationPage}/>
   <Route path="/landing" exact component = {LandingPage}/>
+  <Route path="/employeePage" exact component = {EmployeeProfilePage}/>
+  <Route path="/adminPage" exact component = {AdminProfilePage}/>
 
 </section>
 );
