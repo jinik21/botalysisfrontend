247bfbd11e279d21c9ce6457ead98a628ae66f5d
Ayush Kulshrestha <ayushkulshrestha2001@gmail.com>
2021-08-17 00:42:23 +0530

    delete useless

--- src/components/pages/react-simple-chatbot-master/babel.config.js
+++
@@ -1,8 +0,0 @@
-const presets = ['@babel/env', '@babel/preset-react'];
-const plugins = [
-  '@babel/plugin-transform-arrow-functions',
-  '@babel/plugin-proposal-class-properties',
-  '@babel/plugin-transform-object-assign'
-];
-
-module.exports = { presets, plugins };
