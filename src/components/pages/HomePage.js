95884597707017c916f569e49b02fb11887abab8
kakkarmanan <mr.manankakkar@gmail.com>
2021-08-17 00:37:17 +0530

    Logo.png convert to .svg

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,7 +5,7 @@ import './Logo.png';
 
 const HomePage = () => (
 <div className="ui container">
-  <img src={"./Logo.png"}/>
+  <img src={"./Logo.svg"}/>
   <h1>Welcome</h1>
   <Link to='/login' className="button"> Signin </Link><br/>
   <Link to='/registration' className ="button"> Signup</Link>
