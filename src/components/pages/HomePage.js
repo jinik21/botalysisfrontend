53da82ecce560227a131e42f2d86274c048ece4d
Ayush Kulshrestha <ayushkulshrestha2001@gmail.com>
2021-08-17 00:56:06 +0530

    logo

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,7 +5,7 @@ import './Logo.png';
 
 const HomePage = () => (
 <div className="ui container">
-  <img src={"./Logo.svg"}/>
+  <img style={{height: '75%', width: '75%'}} src={"https://firebasestorage.googleapis.com/v0/b/botalysis.appspot.com/o/Logo.png?alt=media&token=eb52e650-94ba-4c3b-9344-5025455fa0c1"}/>
   <h1>Welcome</h1>
   <Link to='/login' className="button"> Signin </Link><br/>
   <Link to='/registration' className ="button"> Signup</Link>
