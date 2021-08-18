0eff9e91c819d112b9a0f460bf582365e0c4f773
jinik21 <nikheeljain@outlook.com>
2021-08-19 01:46:16 +0530

    adding sentiment data fetch

--- src/components/forms/audioInput.js
+++ src/components/forms/audioInput.js
@@ -1,19 +1,21 @@
 import axios from 'axios';
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import firebase from '../../firebase';
-import { CircularProgressbar, 
+import {
+  CircularProgressbar,
   CircularProgressbarWithChildren,
-  buildStyles } from 'react-circular-progressbar';
+  buildStyles
+} from 'react-circular-progressbar';
 import { withRouter } from 'react-router';
 
 var storageRef = firebase.storage().ref();
-const positivePercentage = 66; 
-const negativePercentage = 14;
-const neutralPercentage = 20;
+var positivePercentage = 66;
+var negativePercentage = 14;
+var neutralPercentage = 20;
 const u = JSON.parse(localStorage.getItem("user"));
 
 const toBlob = (file) => {
-  return new Blob([file],{
+  return new Blob([file], {
     type: 'audio/wav'
   })
 }
@@ -22,137 +24,157 @@ var metadata = {
 };
 
 class AudioInput extends React.Component {
-  
-  constructor(props){
+
+  constructor(props) {
     super(props);
     this.state = {
       selectedFile: null
     }
   };
-    
-    onFileChange = event => {
-      this.setState({ selectedFile: event.target.files[0] });
-    
-    };
-    onFileUpload = async () => {
-      const formData = new FormData();
-      formData.append(
-        "myFile",
-        this.state.selectedFile,
-        this.state.selectedFile.name
-      );
-      console.log(this.state.selectedFile);
-      // axios.post("api/uploadfile", formData);
-      try{
-        const blob = toBlob(this.state.selectedFile);
-        console.log(blob);
-        var mountainsRef = await storageRef.child(this.state.selectedFile.name);
-        await mountainsRef.put(blob,metadata);
-        const res = await mountainsRef.getDownloadURL();
-        console.log(res);
-        // setTimeout(()=>{},5000);
-        fetch('http://localhost:3001/api/process_audio',{
-        method:'post',
-        headers:{'Content-type':'application/json'},
-        body:JSON.stringify({
+
+  onFileChange = event => {
+    this.setState({ selectedFile: event.target.files[0] });
+
+  };
+  onFileUpload = async () => {
+    const formData = new FormData();
+    formData.append(
+      "myFile",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    );
+    console.log(this.state.selectedFile);
+    // axios.post("api/uploadfile", formData);
+    try {
+      const blob = toBlob(this.state.selectedFile);
+      console.log(blob);
+      var mountainsRef = await storageRef.child(this.state.selectedFile.name);
+      await mountainsRef.put(blob, metadata);
+      const res = await mountainsRef.getDownloadURL();
+      console.log(res);
+      // setTimeout(()=>{},5000);
+      fetch('http://localhost:3001/api/process_audio', {
+        method: 'post',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
           email: u.email,
           link: res,
         })
-    })
-        .then(response=>response.json())
-        .then(data=>{
+      })
+        .then(response => response.json())
+        .then(data => {
           console.log(data)
-        })
-        
-      }catch(err){
-        console.log(err)
-      }
-    };
-    fileData = () => {
-    
-      if (this.state.selectedFile) {
-         
-        return (
-          <div>
-            <h2>File Details:</h2>
-             
-            <p>File Name: {this.state.selectedFile.name}</p>          
-                        
-            <p>File Type: {this.state.selectedFile.type}</p>                        
-            <p>
-              Last Modified:{" "}
-              {this.state.selectedFile.lastModifiedDate.toDateString()}
-            </p>
- 
-          </div>
-        );
-      } else {
-        return (
-          <div>
-            <br />
-            <h4>Choose before Pressing the Upload button</h4>
-          </div>
-        );
-      }
-    };
-    
-    render() {
-    
-      return (
-        <div> 
-            <div>
-                <input type="file" onChange={this.onFileChange} className = "audioInputFile" />
-                <button onClick={this.onFileUpload} className = "uploadButton">
-                  Upload!
-                </button>
-            </div>
-          {this.fileData()}
-          <div>
-            <table className = "reportTable">
-              <tr>
-              <th><CircularProgressbar
-                    className = "percentageBar" 
-                    value={positivePercentage} 
-                    text={`${positivePercentage}%`}
-                    styles={buildStyles({
-                      strokeLinecap: 'butt',
-                      textSize: '16px',
-                      pathTransitionDuration: 0.5,
-                      pathColor: '#92fb61',
-                      textColor: '#92fb61',
-                      backgroundColor: '#E0D2D0',
-                    })} /></th>
-              <th><CircularProgressbar 
-                    value={neutralPercentage} 
-                    text={`${neutralPercentage}%`}
-                    styles={buildStyles({
-                      strokeLinecap: 'butt',
-                      textSize: '16px',
-                      pathTransitionDuration: 0.5,
-                      pathColor: '#d6d6d6',
-                      textColor: '#d6d6d6',
-                      backgroundColor: '#E0D2D0',
-                    })} /></th>
-              <th><CircularProgressbar 
-                      value={negativePercentage} 
-                      text={`${negativePercentage}%`}
-                      styles={buildStyles({
-                        strokeLinecap: 'butt',
-                        textSize: '16px',
-                        pathTransitionDuration: 0.5,
-                        pathColor: '#f88',
-                        textColor: '#f88',
-                        backgroundColor: '#E0D2D0',
-                      })} /></th>
-              </tr>
-            </table>
-          </div>
-        </div>
+          setTimeout(()=>{
+            fetch('http://localhost:3001/api/sentiment', {
+            method: 'post',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({
+              email: u.email,
+            })
+          })
+            .then(response => response.json())
+            .then(data => {
 
+              positivePercentage = data.positive;
+              negativePercentage = data.negative;
+              neutralPercentage = data.neutral;
+              console.log(data);
+            })
+
+          },5000);
+          
+        })
+
+
+    } catch (err) {
+      console.log(err)
+    }
+  };
+  fileData = () => {
+
+    if (this.state.selectedFile) {
+
+      return (
+        <div>
+          <h2>File Details:</h2>
+
+          <p>File Name: {this.state.selectedFile.name}</p>
+
+          <p>File Type: {this.state.selectedFile.type}</p>
+          <p>
+            Last Modified:{" "}
+            {this.state.selectedFile.lastModifiedDate.toDateString()}
+          </p>
+
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+        </div>
       );
     }
-    
+  };
 
+  render() {
+
+    return (
+      <div>
+        <div>
+          <input type="file" onChange={this.onFileChange} className="audioInputFile" />
+          <button onClick={this.onFileUpload} className="uploadButton">
+            Upload!
+          </button>
+        </div>
+        {this.fileData()}
+        <div>
+          <table className="reportTable">
+            <tr>
+              <th><CircularProgressbar
+                className="percentageBar"
+                value={positivePercentage}
+                text={`${positivePercentage}%`}
+                styles={buildStyles({
+                  strokeLinecap: 'butt',
+                  textSize: '16px',
+                  pathTransitionDuration: 0.5,
+                  pathColor: '#92fb61',
+                  textColor: '#92fb61',
+                  backgroundColor: '#E0D2D0',
+                })} /></th>
+              <th><CircularProgressbar
+                value={neutralPercentage}
+                text={`${neutralPercentage}%`}
+                styles={buildStyles({
+                  strokeLinecap: 'butt',
+                  textSize: '16px',
+                  pathTransitionDuration: 0.5,
+                  pathColor: '#d6d6d6',
+                  textColor: '#d6d6d6',
+                  backgroundColor: '#E0D2D0',
+                })} /></th>
+              <th><CircularProgressbar
+                value={negativePercentage}
+                text={`${negativePercentage}%`}
+                styles={buildStyles({
+                  strokeLinecap: 'butt',
+                  textSize: '16px',
+                  pathTransitionDuration: 0.5,
+                  pathColor: '#f88',
+                  textColor: '#f88',
+                  backgroundColor: '#E0D2D0',
+                })} /></th>
+            </tr>
+          </table>
+        </div>
+      </div>
+
+    );
   }
- 
-  export default withRouter(AudioInput);
+
+
+}
+
+export default withRouter(AudioInput);
